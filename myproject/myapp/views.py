30932e5c7b885563d22a7bd166afbd36e2bc7c7e
EC2 Default User <ec2-user@ip-172-31-45-221.eu-west-1.compute.internal>
2024-03-30 22:58:17 +0000

    updated views file and created register page

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 # Create your views here.
+
+# - Homepage 
 
 def home(request):
     
     return render(request, 'myapp/index.html')
+    
+
+# - Register a user
+
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            
+
+            # return redirect("login")
+
+    context = {'form':form}
+
+    return render(request, 'myapp/register.html', context=context)
+
